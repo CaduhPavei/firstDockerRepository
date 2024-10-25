4522bf2550575a06f33d055acb46f95fd63367d7
CaduhPavei <CaduhPavei@gmail.com>
2024-10-25 20:28:21 -0300

    First COmmit

---
+++ src/main/java/com/example/docker_teste/DockerTesteApplication.java
@@ -0,0 +1,20 @@
+package com.example.docker_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class 	DockerTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTesteApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String seyHello(){
+        return "Hello!";
+    }
+}
